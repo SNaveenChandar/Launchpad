34cb3dfc990f7da0d935d5c26f4e7c287237a73b
Naveen Chandar <nchandar>
2024-07-29 10:51:18 +0000

    Updated new links

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,118 +1,10 @@
 sap.ui.define(
   [
     "sap/ui/core/mvc/Controller",
-    "sap/m/library",
     "sap/ui/model/json/JSONModel",
   ],
-  function (Controller, mobileLibrary, JSONModel) {
+  function (Controller, JSONModel) {
     "use strict";
-    const "url"Helper = mobileLibrary."url"Helper;
-
-    // const Apps = {
-
-    //     "RFS2Specific": [
-    //                 {
-    //                   ""applicationName"": "Maintain RFS2 Materials",
-    //                   ""url"": "https://chsincdev.launchpad.cfapps.us10.hana.ondemand.com/6d0d9d02-1710-4b5a-b05f-526ca8a4c038.comsapchscomRegCompMaster.comsapchscomzcomfemaintainrfs2materials-0.0.1"
-    //                 }
-    //           ],
-    //             "Transactional": [
-    //                 {
-    //                   ""applicationName"": "Renewable Workplace",
-    //                   ""url"": "https://chsincdev.launchpad.cfapps.us10.hana.ondemand.com/81177949-b9b3-476f-bd60-69a809044398.zfsrenewwrkplc.zfsrenewwrkplc-0.0.1/index.html"
-    //                 },
-    //                 {
-    //                     ""applicationName"": "Manual Adjustment",
-    //                     ""url"": "https://chsincdev.launchpad.cfapps.us10.hana.ondemand.com/21c4dfe8-a0d2-4536-ad86-e6c8f00d1920.zfsmanadjui.zfsmanadjui-0.0.1/index.html#"
-    //                 },
-    //                 {
-    //                     ""applicationName"": "Log Utility",
-    //                     ""url"": "https://CHSIncDev.launchpad.cfapps.us10.hana.ondemand.com/8a3eb9ae-d209-4624-8ac9-48a9ab963702.comsapchscomlogutilityservice.comsapchscomlogutilityservicezcomfelogutilityservice-0.0.1"
-
-    //                 },
-    //             ],
-    //             "Technical": [
-    //                 {
-    //                     ""applicationName"": "Maintain Regulation Types",
-    //                     ""url"": "https://chsincdev.launchpad.cfapps.us10.hana.ondemand.com/6d0d9d02-1710-4b5a-b05f-526ca8a4c038.comsapchscomRegCompMaster.comsapchscomzcomfemaintainregulationtype-0.0.1"
-    //                 },
-    //                 {
-    //                     ""applicationName"": "Maintain Transaction Types",
-    //                     ""url"": "https://chsincdev.launchpad.cfapps.us10.hana.ondemand.com/6d0d9d02-1710-4b5a-b05f-526ca8a4c038.comsapchscomRegCompMaster.comsapchscomzcomfemaintaintransactiontype-0.0.1"
-    //                 },
-    //                 {
-    //                     ""applicationName"": "Maintain Regulation Transaction Types",
-    //                     ""url"": "https://chsincdev.launchpad.cfapps.us10.hana.ondemand.com/6d0d9d02-1710-4b5a-b05f-526ca8a4c038.comsapchscomRegCompMaster.comsapchscomzcomfemaintainregulationtransactiontypets-0.0.1"
-    //                 },
-    //                 {
-    //                     ""applicationName"": "Maintain Regulation Object Types",
-    //                     ""url"": "https://chsincdev.launchpad.cfapps.us10.hana.ondemand.com/6d0d9d02-1710-4b5a-b05f-526ca8a4c038.comsapchscomRegCompMaster.comsapchscomzcomfemaintainregulationobjecttype-0.0.1"
-    //                 },
-    //                 {
-    //                     ""applicationName"": "Maintain Action Sub-Scenario Mapping",
-    //                     ""url"": "https://chsincdev.launchpad.cfapps.us10.hana.ondemand.com/6d0d9d02-1710-4b5a-b05f-526ca8a4c038.comsapchscomRegCompMaster.comsapchscomzcomfemaintainregulationsubscenarioaction-0.0.1"
-    //                 },
-    //                 {
-    //                     ""applicationName"": "Maintain Regulation Sub Scenario To Scenario Assignment",
-    //                     ""url"": "https://chsincdev.launchpad.cfapps.us10.hana.ondemand.com/6d0d9d02-1710-4b5a-b05f-526ca8a4c038.comsapchscomRegCompMaster.comsapchscomzcomfemaintainregulationsubscenariotoscenario-0.0.1"
-    //                 },
-    //                 {
-    //                     ""applicationName"": "Maintain ID Range For Renewable Objects",
-    //                     ""url"": "https://CHSIncDev.launchpad.cfapps.us10.hana.ondemand.com/6d0d9d02-1710-4b5a-b05f-526ca8a4c038.comsapchscomRegCompMaster.comsapchscomzcomfemaintainidrangeforrenewableobjects-0.0.1"
-    //                 },
-    //                 {
-    //                     ""applicationName"": "Maintain ID Range For Reconciliation Groups",
-    //                     ""url"": "https://CHSIncDev.launchpad.cfapps.us10.hana.ondemand.com/6d0d9d02-1710-4b5a-b05f-526ca8a4c038.comsapchscomRegCompMaster.comsapchscomzcomfemaintainidrangeforreconciliationgroups-0.0.1"
-    //                 }
-
-    //             ],
-    //             "Business": [ {
-    //                 ""applicationName"": "Maintain Regulation Groups",
-    //                 ""url"": "https://CHSIncDev.launchpad.cfapps.us10.hana.ondemand.com/6d0d9d02-1710-4b5a-b05f-526ca8a4c038.comsapchscomRegCompMaster.comsapchscomzcomfemaintainregulationgroup-0.0.1"
-
-    //             },
-    //             {
-    //                 ""applicationName"": "Maintain Regulation Material Groups",
-    //                 ""url"": "https://CHSIncDev.launchpad.cfapps.us10.hana.ondemand.com/6d0d9d02-1710-4b5a-b05f-526ca8a4c038.comsapchscomRegCompMaster.comsapchscomzcomfemaintainregulationmaterialgroup-0.0.1"
-
-    //             },{
-    //                 ""applicationName"": "Maintain Movement Type Relevancy For Scenario",
-    //                 ""url"": "https://chsincdev.launchpad.cfapps.us10.hana.ondemand.com/6d0d9d02-1710-4b5a-b05f-526ca8a4c038.comsapchscomRegCompMaster.comsapchscomzcomfemaintainmovementtype-0.0.1"
-
-    //             },{
-    //                 ""applicationName"": "Maintain Movement Type To Transaction Category Mapping",
-    //                 ""url"": "https://chsincdev.launchpad.cfapps.us10.hana.ondemand.com/6d0d9d02-1710-4b5a-b05f-526ca8a4c038.comsapchscomRegCompMaster.comsapchscomzcomfemaintainmovementtypetoteansactioncategorymapping-0.0.1"
-
-    //             }, {
-    //                 ""applicationName"": "Maintain Regulation Transaction Types With Impact",
-    //                 ""url"": "https://CHSIncDev.launchpad.cfapps.us10.hana.ondemand.com/6d0d9d02-1710-4b5a-b05f-526ca8a4c038.comsapchscomRegCompMaster.comsapchscomzcomfemaintainregulationtransactiontypebu-0.0.1"
-
-    //             },{
-    //                 ""applicationName"": "Maintain Incoterms Impact For Import / Export",
-    //                 ""url"": "https://CHSIncDev.launchpad.cfapps.us10.hana.ondemand.com/6d0d9d02-1710-4b5a-b05f-526ca8a4c038.comsapchscomRegCompMaster.comsapchscomzcomfemaintainincotermsimpactforimportorexport-0.0.1"
-
-    //             },{
-    //                 ""applicationName"": "Maintain Inventory Posting Plant",
-    //                 ""url"": "https://CHSIncDev.launchpad.cfapps.us10.hana.ondemand.com/6d0d9d02-1710-4b5a-b05f-526ca8a4c038.comsapchscomRegCompMaster.comsapchscomzcomfemaintaininventorypostingplant-0.0.1"
-
-    //             },{
-    //                 ""applicationName"": "Maintain Company Code And Plant Mapping",
-    //                 ""url"": "https://chsincdev.launchpad.cfapps.us10.hana.ondemand.com/6d0d9d02-1710-4b5a-b05f-526ca8a4c038.comsapchscomRegCompMaster.comsapchscomzcomfemaintaincompanyidtoplant-0.0.1"
-    //            },{
-    //                 ""applicationName"": "Maintain Adjustment Reason Codes",
-    //                 ""url"": "https://CHSIncDev.launchpad.cfapps.us10.hana.ondemand.com/6d0d9d02-1710-4b5a-b05f-526ca8a4c038.comsapchscomRegCompMaster.comsapchscomzcomfemaintainadjustmentreasoncode-0.0.1"
-
-    //             },  {
-    //                 ""applicationName"": "Maintain Date Tolerance For Auto-Match",
-    //                 ""url"": "https://CHSIncDev.launchpad.cfapps.us10.hana.ondemand.com/6d0d9d02-1710-4b5a-b05f-526ca8a4c038.comsapchscomRegCompMaster.comsapchscomzcomfemaintainregulationtolerancefordatematch-0.0.1"
-
-    //             },{
-    //                 ""applicationName"": "Maintain Volume Tolerance For Auto-Match",
-    //                 ""url"": "https://CHSIncDev.launchpad.cfapps.us10.hana.ondemand.com/6d0d9d02-1710-4b5a-b05f-526ca8a4c038.comsapchscomRegCompMaster.comsapchscomzcomfemaintainregulatontoleranceforvolumematch-0.0.1"
-
-    //             }
-    //             ]
-    //         };
     const Apps = {
       "RFS2Specific": [
         {
@@ -230,7 +122,7 @@ sap.ui.define(
           "applicationName": "Maintain Auto-match Volume Tolerance",
           "url": " https://chsincdev.launchpad.cfapps.us10.hana.ondemand.com/80062591-0c3c-4272-a622-b05a7276bd7d.comchssapzcom_mtnregtol4volmtch.zfemtnregtol4volmtch-0.0.1",
         },
-      ],
+      ]
     };
     return Controller.extend("com.chs.launchpad.controller.Home", {
       onInit: function () {
@@ -238,7 +130,7 @@ sap.ui.define(
         this.getView().setModel(oModel);
       },
       onPressTile: function (surl) {
-        urlHelper.redirect(surl, false);
+        sap.m.URLHelper.redirect(surl, false);
       },
     });
   }
