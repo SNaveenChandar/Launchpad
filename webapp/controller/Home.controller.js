f5995d4b01e179d7b11073c66f75755cb697c235
Lakshmana Lakshmana <llakshma>
2024-10-15 07:26:37 +0000

    Added tile for Auto Match Configuration

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -15,6 +15,10 @@ sap.ui.define(
           "applicationName": "Maintain RVO Type To RIN Type Mapping",
           "url": "https://chsincdev.launchpad.cfapps.us10.hana.ondemand.com/82ac4c72-de64-466d-89a9-990e8fd516d3.comchssapzcom_mtnrvotyprintypmp.zfemtnrvotyprintypmp-0.0.1",
         },
+        {
+          "applicationName": "Auto Match Configuration",
+          "url": "https://chsincdev.launchpad.cfapps.us10.hana.ondemand.com/0760753d-98bd-4ddc-b294-0887a9528738.comchssapzcom_mtnatmtchconfg.zfemtnatmtchconfg-0.0.1/index.html",
+        },
       ],
       "Transactional": [
         {
